354cc9c40de617c72129118c5412df812f24a7e4
DonMck <donmck@amazon.com>
2022-02-18 12:47:37 -0600

    Initial Commit

---
+++ Shared/MastermindsBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MastermindsBookApp.swift
+//  Shared
+//
+//  Created by Don McKenzie on 18-Feb-22.
+//
+
+import SwiftUI
+
+@main
+struct MastermindsBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
